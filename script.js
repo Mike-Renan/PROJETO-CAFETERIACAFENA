282763fc13f3f3465c885d245660a318f047d2c0
Mike-Renan <mike.renan@yahoo.com.br>
2025-06-26 20:57:59 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+  const toggle = document.getElementById('menu-toggle');
+  const navbar = document.querySelector('.navbar');
+
+  toggle.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+  });
+
