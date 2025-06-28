04317d724b3dcaed445943b4f57cdfd86f60b5d6
Mike-Renan <mike.renan@yahoo.com.br>
2025-06-28 09:40:28 -0300

    menu H

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const navbar = document.querySelector(".navbar");
+const navLinks = navbar.querySelectorAll("a");
 
-  const toggle = document.getElementById('menu-toggle');
-  const navbar = document.querySelector('.navbar');
+// Fecha o menu ao clicar em um link
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("active");
+  navbar.classList.toggle("active");
+});
 
-  toggle.addEventListener('click', () => {
-    navbar.classList.toggle('active');
+
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    menuToggle.classList.remove("active");
+    navbar.classList.remove("active");
   });
-
+});
